b0bbcde5e79458d2d60c9a3ecc35b2d362e0963b
ArielInguillay <arieling123@gmail.com>
2025-05-23 01:44:58 -0500

    Varios cambios

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import Link from 'next/link';
-
-export default function Footer() {
-  return (
-    <footer style={{ backgroundColor: '#144', padding: '2rem', color: 'white' }}>
-      <div style={{ display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap' }}>
-        <div>
-          <p>Hecho con ❤️ en Ecuador</p>
-          <p>Este sitio es de acceso libre y educativo</p>
-        </div>
-        <div>
-          <strong>Mapa del Sitio</strong><br />
-          <Link href="/equipo">Equipo</Link><br />
-          <Link href="/metodologia">Metodología</Link><br />
-          <Link href="/entrevistas">Entrevistas</Link><br />
-          <Link href="/difunde">Difunde</Link><br />
-          <Link href="/recursos">Recursos</Link>
-        </div>
-        <div>
-          <strong>Redes Sociales</strong><br />
-          <p>Twitter</p>
-          <p>TikTok</p>
-          <p>Youtube</p>
-          <p>Facebook</p>
-          <p>Instagram</p>
-=======
 "use client";
 import Link from "next/link";
 import { FaTiktok, FaYoutube, FaFacebook, FaInstagram } from "react-icons/fa6";
@@ -32,33 +5,29 @@ import { BsTwitterX } from "react-icons/bs";
 
 export default function Footer() {
   return (
-    <footer className="bg-[#2F4F4F] text-white px-6 py-10">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
-        {/* Logo y mensaje */}
-        <div className="flex flex-col items-start space-y-2">
-          <h1 className="text-yellow-300 text-2xl font-bold tracking-wide">
-            IDENTIDAD <span className="text-white">RURAL</span>
-          </h1>
-          <p>Hecho con <span className="text-red-500">❤️</span> en Ecuador</p>
-          <p className="text-gray-300">Este sitio es de acceso libre y educativo</p>
-        </div>
-
-        {/* Mapa del sitio */}
-        <div>
-          <h2 className="text-lg font-semibold mb-2">Mapa del Sitio</h2>
-          <ul className="space-y-1 text-gray-200">
-            <li><Link href="/Equipo" className="hover:text-yellow-300">Equipo</Link></li>
-            <li><Link href="/Metodologia" className="hover:text-yellow-300">Metodología</Link></li>
-            <li><Link href="/Entrevistas" className="hover:text-yellow-300">Entrevistas</Link></li>
-            <li><Link href="/Difunde" className="hover:text-yellow-300">Difunde</Link></li>
-            <li><Link href="/Recursos" className="hover:text-yellow-300">Recursos</Link></li>
-          </ul>
-        </div>
-
-        {/* Redes sociales */}
-        <div>
-          <h2 className="text-lg font-semibold mb-2">Redes Sociales</h2>
-          <ul className="space-y-2 text-gray-200">
+    <footer className="bg-[#476350] text-white px-6 py-10">
+<div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-5 gap-8">
+  {/* Logo y mensaje */}
+  <div className="flex flex-col items-start space-y-2 md:col-span-3">
+    <h1 className="text-white-300 text-3xl font-heading font-bold tracking-wide">
+      IDENTIDAD <span className="text-white">RURAL</span>
+    </h1>
+    <p>Hecho con <span className="text-red-500">❤️</span> en Ecuador</p>
+    <p className="text-gray-300">Este sitio es de acceso libre y educativo</p>
+  </div>
+  <div>
+    <h2 className="text-lg font-semibold mb-2 border-b border-white pb-1">Recursos Rurales</h2>
+    <ul className="space-y-2 text-gray-200">
+      <li><Link href="/Datos_Rurales" className="hover:text-teal-300">Datos Rurales</Link></li>
+      <li><Link href="/Datos_Curiosos" className="hover:text-teal-300">Datos Curiosos</Link></li>
+      <li><Link href="/Problemas" className="hover:text-teal-300">Problemas</Link></li>
+      <li><Link href="/Voz_Rural" className="hover:text-teal-300">Voz Rural</Link></li>
+      <li><Link href="/Entrevistas" className="hover:text-teal-300">Entrevistas</Link></li>
+    </ul>
+  </div>
+  <div>
+    <h2 className="text-lg font-semibold mb-2 border-b border-white pb-1">Redes Sociales</h2>
+    <ul className="space-y-2 text-gray-200">
             <li className="flex items-center gap-2">
               <a href="https://x.com/ConagopareN" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 hover:text-black">
                 <BsTwitterX/> <span>X</span>
@@ -85,13 +54,8 @@ export default function Footer() {
               </a>
             </li>
           </ul>
->>>>>>> kevin
         </div>
       </div>
     </footer>
   );
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> kevin
