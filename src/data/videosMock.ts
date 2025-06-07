3aac39336fab03ce85045f17dab01934379ef318
ArielInguillay <arieling123@gmail.com>
2025-06-07 16:44:20 -0500

    Entrevistas - Se agregaron mas videos

--- src/data/videosMock.ts
+++ src/data/videosMock.ts
@@ -59,4 +59,542 @@ export const videosMock: Video[] = [
     videoId: "UblbbZxrZQk",
     titulo: "Gobierno Parroquial San José de Pózul"
   },
+  {
+    id: 7,
+    provincia: "Carchi",
+    canton: "Bolívar",
+    parroquia: "Monte Olivo",
+    videoId: "Six34QHAsqE",
+    titulo: "Gobierno Parroquial Monte Olivo"
+  },
+  {
+    id: 8,
+    provincia: "Azuay",
+    canton: "Sígsig",
+    parroquia: "Güel",
+    videoId: "fEKILYcBMq0",
+    titulo: "Gobierno Parroquial Guel"
+  },
+  {
+    id: 9,
+    provincia: "Loja",
+    canton: "Loja",
+    parroquia: "San Lucas",
+    videoId: "N-v7aDoC8Ek",
+    titulo: "Gobierno Parroquial San Lucas"
+  },
+  {
+    id: 10,
+    provincia: "Tungurahua",
+    canton: "Ambato",
+    parroquia: "San Fernando",
+    videoId: "uCfK8yXpOoQ",
+    titulo: "Gobierno Parroquial San Fernando"
+  },
+  {
+    id: 11,
+    provincia: "Tungurahua",
+    canton: "Ambato",
+    parroquia: "SHuachi Grande",
+    videoId: "-TCwFXYiAcU",
+    titulo: "Gobierno Parroquial Huachi Grande"
+  },
+  {
+    id: 12,
+    provincia: "Morona Santiago",
+    canton: "San Juan Bosco",
+    parroquia: "San Jacinto de Wakambeis",
+    videoId: "OlCEfewHTsU",
+    titulo: "Gobierno Parroquial San Jacinto de Wakambeis"
+  },
+  {
+    id: 13,
+    provincia: "Carchi",
+    canton: "Tulcán",
+    parroquia: "Pioter",
+    videoId: "KmzhnarqcC4",
+    titulo: "Gobierno Parroquial Pioter"
+  },
+  {
+    id: 14,
+    provincia: "Imbabura",
+    canton: "Cotacachi",
+    parroquia: "Peñaherrera",
+    videoId: "b7f9KSJyslQ",
+    titulo: "Gobierno Parroquial Peñaherrera"
+  },
+  {
+    id: 15,
+    provincia: "Tungurahua",
+    canton: "Ambato",
+    parroquia: "Augusto N. Martínez",
+    videoId: "foDR5S0I96Y",
+    titulo: "Gobierno Parroquial Augusto N. Martínez"
+  },
+  {
+    id: 16,
+    provincia: "Tungurahua",
+    canton: "Ambato",
+    parroquia: "San Antonio de Quisapincha",
+    videoId: "flDG4GFehgk",
+    titulo: "Gobierno Parroquial Quisapincha"
+  },
+  {
+    id: 17,
+    provincia: "Tungurahua",
+    canton: "Ambato",
+    parroquia: "Constantino Fernández",
+    videoId: "61trtsRp4Q0",
+    titulo: "Gobierno Parroquial Constantino Fernández"
+  },
+  {
+    id: 18,
+    provincia: "Tungurahua",
+    canton: "Ambato",
+    parroquia: "Juan Benigno Vela",
+    videoId: "tb07iiCaNrY",
+    titulo: "Gobierno Parroquial Juán Benigno Vela"
+  },
+  {
+    id: 19,
+    provincia: "Tungurahua",
+    canton: "Ambato",
+    parroquia: "Atahualpa",
+    videoId: "pvv39GB94PA",
+    titulo: "Gobierno Parroquial Atahualpa"
+  },
+
+  {
+    id: 20,
+    provincia: "Tungurahua",
+    canton: "Quero",
+    parroquia: "Yanayacu",
+    videoId: "DumFLRKSOzI",
+    titulo: "Gobierno Parroquial Yanayacu"
+  },
+{
+    id: 21,
+    provincia: "Tungurahua",
+    canton: "Santiago de Píllaro",
+    parroquia: "Presidente Urbina (Chagrapamba - Patzucul)",
+    videoId: "AXnKaCifShs",
+    titulo: "Gobierno Parroquial Urbina"
+  },
+
+{
+    id: 22,
+    provincia: "Tungurahua",
+    canton: "Baños de Agua Santa",
+    parroquia: "Ulba",
+    videoId: "k8XZ0A_-PwY",
+    titulo: "Gobierno Parroquial Ulba"
+  },
+
+  {
+    id: 23,
+    provincia: "Tungurahua",
+    canton: "Ambato",
+    parroquia: "Unamuncho",
+    videoId: "OXd-uhlpMe4",
+    titulo: "Gobierno Parroquial Unamuncho"
+  },
+
+    {
+    id: 24,
+    provincia: "Tungurahua",
+    canton: "Ambato",
+    parroquia: "Totoras",
+    videoId: "2jWPW9lIS0w",
+    titulo: "Gobierno Parroquial Totoras"
+  },
+
+    {
+    id: 25,
+    provincia: "Tungurahua",
+    canton: "Ambato",
+    parroquia: "Santa Rosa",
+    videoId: "3LL5fGz6EeU",
+    titulo: "Gobierno Parroquial Santa Rosa"
+  },
+
+    {
+    id: 26,
+    provincia: "Tungurahua",
+    canton: "Ambato",
+    parroquia: "Izamba",
+    videoId: "YdgXsNiuIaE",
+    titulo: "Gobierno Parroquial Izamba"
+  },
+
+    {
+    id: 27,
+    provincia: "Morona Santiago",
+    canton: "Morona",
+    parroquia: "General Proaño",
+    videoId: "lF_xft0qkIs",
+    titulo: "Gobierno Parroquial Gral. Proaño"
+  },
+
+    {
+    id: 28,
+    provincia: "Morona Santiago",
+    canton: "Taisha",
+    parroquia: "Tuutinentza",
+    videoId: "WOn6utbBgww",
+    titulo: "Gobierno Parroquial Tuutinentza"
+  },
+
+    {
+    id: 29,
+    provincia: "Morona Santiago",
+    canton: "Morona",
+    parroquia: "Sinaí",
+    videoId: "_kaz_LLerQo",
+    titulo: "Gobierno Parroquial Sinaí"
+  },
+
+    {
+    id: 30,
+    provincia: "Morona Santiago",
+    canton: "Logroño",
+    parroquia: "Yaupi",
+    videoId: "HYP6X3KHpyk",
+    titulo: "Gobierno Parroquial Yaupi"
+  },
+
+    {
+    id: 31,
+    provincia: "Morona Santiago",
+    canton: "Limón Indanza",
+    parroquia: "San Miguel de Conchay",
+    videoId: "VqEznVr8zlY",
+    titulo: "Gobierno Parroquial San Miguel de Conchay"
+  },
+
+    {
+    id: 32,
+    provincia: "Morona Santiago",
+    canton: "Gualaquiza",
+    parroquia: "El Rosario",
+    videoId: "a2eiK8bJb40",
+    titulo: "Gobierno Parroquial El Rosario"
+  },
+
+    {
+    id: 33,
+    provincia: "Morona Santiago",
+    canton: "Gualaquiza",
+    parroquia: "Bermejos",
+    videoId: "03TAs2t6Glo",
+    titulo: "Gobierno Parroquial Bermejos"
+  },
+
+    {
+    id: 34,
+    provincia: "Loja",
+    canton: "Sozoranga",
+    parroquia: "Nueva Fátima",
+    videoId: "avJ46i4Vi08",
+    titulo: "Gobierno Parroquial Nueva Fátima"
+  },
+
+    {
+    id: 35,
+    provincia: "Loja",
+    canton: "Saraguro",
+    parroquia: "Lluzhapa",
+    videoId: "oCriUvBQbSs",
+    titulo: "Gobierno Parroquial Lluzhapa"
+  },
+
+    {
+    id: 36,
+    provincia: "Loja",
+    canton: "Puyango",
+    parroquia: "El Limo",
+    videoId: "D4l8wKI4nws",
+    titulo: "Gobierno Parroquial El Limo"
+  },
+
+    {
+    id: 37,
+    provincia: "Loja",
+    canton: "Paltas",
+    parroquia: "Cangonamá",
+    videoId: "PbD0DRxPCcE",
+    titulo: " Gobierno Parroquial Cangonama"
+  },
+
+    {
+    id: 38,
+    provincia: "Loja",
+    canton: "Loja",
+    parroquia: "San Pedro de Vilcabamba",
+    videoId: "ldWj88Lpagg",
+    titulo: "Gobierno Parroquial San Pedro de Vilcabamba"
+  },
+
+    {
+    id: 39,
+    provincia: "Loja",
+    canton: "Chaguarpamba",
+    parroquia: "El Rosario",
+    videoId: "BpUGXq8H5Bk",
+    titulo: "Gobierno Parroquial El Rosario"
+  },
+
+    {
+    id: 40,
+    provincia: "Loja",
+    canton: "Chaguarpamba",
+    parroquia: "Amarillos",
+    videoId: "UKg9ogFrQJg",
+    titulo: "Gobierno Parroquial Amarillos"
+  },
+
+    {
+    id: 41,
+    provincia: "Loja",
+    canton: "Celica",
+    parroquia: "Teniente Maximiliano Rodríguez Loaiza",
+    videoId: "qZNo53rrgLE",
+    titulo: "Gobierno Parroquial Tnte. Maximiliano Rodríguez Loaiza"
+  },
+
+    {
+    id: 42,
+    provincia: "Loja",
+    canton: "Celica",
+    parroquia: "San Juan de Pózul",
+    videoId: "ybvtz7ojBPU",
+    titulo: "Gobierno Parroquial Pózul"
+  },
+
+    {
+    id: 43,
+    provincia: "Loja",
+    canton: "Saraguro",
+    parroquia: "Sumaypamba",
+    videoId: "vVJMEIonfSk",
+    titulo: "Gobierno Parroquial Sumaypamba"
+  },
+
+    {
+    id: 44,
+    provincia: "Loja",
+    canton: "Espíndola",
+    parroquia: "El Ingenio",
+    videoId: "3CTlMj6XJes",
+    titulo: "Gobierno Parroquial El Ingenio"
+  },
+
+    {
+    id: 45,
+    provincia: "Imbabura",
+    canton: "San Miguel de Urcuquí",
+    parroquia: "La Merced de Buenos Aires",
+    videoId: "Vu2ZwyPOCoA",
+    titulo: "Gobierno Parroquial La Merced de Buenos Aires"
+  },
+
+    {
+    id: 46,
+    provincia: "Imbabura",
+    canton: "Cotacachi",
+    parroquia: "Vacas Galindo",
+    videoId: "52MG2iHJZ6M",
+    titulo: "Gobierno Parroquial Vacas Galindo"
+  },
+
+    {
+    id: 47,
+    provincia: "Imbabura",
+    canton: "Otavalo",
+    parroquia: "Eugenio Espejo",
+    videoId: "6cDlzT2BqVI",
+    titulo: "Gobierno Parroquial Eugenio Espejo"
+  },
+
+    {
+    id: 48,
+    provincia: "Imbabura",
+    canton: "Otavalo",
+    parroquia: "6 de Julio de Cuellaje",
+    videoId: "MGHcukw1GvA",
+    titulo: "Gobierno Parroquial 6 de Julio de Cuellaje"
+  },
+
+    {
+    id: 49,
+    provincia: "Carchi",
+    canton: "Tulcán",
+    parroquia: "Urbina",
+    videoId: "fTkABQC1SZQ",
+    titulo: "Gobierno Parroquial Urbina"
+  },
+
+  
+    {
+    id: 50,
+    provincia: "Carchi",
+    canton: "Tulcán",
+    parroquia: "Julio Andrade",
+    videoId: "P6tdVrL4T3U",
+    titulo: "Gobierno Parroquial Julio Andrade"
+  },
+
+  
+    {
+    id: 51,
+    provincia: "Carchi",
+    canton: "Montúfar",
+    parroquia: "La Paz",
+    videoId: "owmBN6_IyRg",
+    titulo: "Gobierno Parroquial La Paz"
+  },
+
+  
+    {
+    id: 52,
+    provincia: "Carchi",
+    canton: "Montúfar",
+    parroquia: "Chitán de Navarretes",
+    videoId: "Gl_QRyC6hGM",
+    titulo: "Gobierno Parroquial Chitán de Navarretes"
+  },
+
+  
+    {
+    id: 53,
+    provincia: "Carchi",
+    canton: "Mira",
+    parroquia: "La Concepción",
+    videoId: "rNEKNdP1Ob4",
+    titulo: "Gobierno Parroquial La Concepción"
+  },
+
+  
+    {
+    id: 54,
+    provincia: "Carchi",
+    canton: "Espejo",
+    parroquia: "San Isidro",
+    videoId: "hmgQI06zdkM",
+    titulo: "Gobierno Parroquial San Isidro"
+  },
+
+  {
+    id: 55,
+    provincia: "Azuay",
+    canton: "Sígsig",
+    parroquia: "Jima",
+    videoId: "HNjrKeGgET8",
+    titulo: "Gobierno Parroquial Jima"
+  },
+
+
+  {
+    id: 56,
+    provincia: "Azuay",
+    canton: "Sígsig",
+    parroquia: "San José de Raranga",
+    videoId: "m6i17_Icn88",
+    titulo: "Gobierno Parroquial San José de Raranga"
+  },
+
+
+  {
+    id: 57,
+    provincia: "Azuay",
+    canton: "Paute",
+    parroquia: "Tomebamba",
+    videoId: "Hkje_xQbZ6I",
+    titulo: "Gobierno Parroquial Tomebamba"
+  },
+
+
+  {
+    id: 58,
+    provincia: "Azuay",
+    canton: "Paute",
+    parroquia: "El Cabo",
+    videoId: "1uD7n4Y8I5s",
+    titulo: "Gobierno Parroquial El Cabo"
+  },
+
+
+  {
+    id: 59,
+    provincia: "Azuay",
+    canton: "Nabón",
+    parroquia: "El Progreso",
+    videoId: "lo2CCAGRX0E",
+    titulo: "Gobierno Parroquial El Progreso"
+  },
+
+  {
+    id: 60,
+    provincia: "Azuay",
+    canton: "Nabón",
+    parroquia: "Cochapata",
+    videoId: "ko6QvVt6w-A",
+    titulo: "Gobierno Parroquial Cochapata"
+  },
+
+
+  {
+    id: 61,
+    provincia: "Azuay",
+    canton: "Cuenca",
+    parroquia: "Santa Ana",
+    videoId: "2M00p_w8b-I",
+    titulo: "Gobierno Parroquial Santa Ana"
+  },
+
+  {
+    id: 62,
+    provincia: "Azuay",
+    canton: "Camilo Ponce Enríquez",
+    parroquia: "El Carmen de Pijilí",
+    videoId: "2r8nBdylF2c",
+    titulo: "Gobierno Parroquial El Carmen de Pijilí"
+  },
+
+  {
+    id: 63,
+    provincia: "Cañar",
+    canton: "La Troncal",
+    parroquia: "Manuel de Jesús Calle",
+    videoId: "VRC8fvAc3ys",
+    titulo: "Gobierno Parroquial Manuel de Jesús Calle"
+  },
+
+  {
+    id: 64,
+    provincia: "Cañar",
+    canton: "Cañar",
+    parroquia: "Zhud",
+    videoId: "Dz4A2-i3GlE",
+    titulo: "Gobierno Parroquial Zhud"
+  },
+
+  {
+    id: 65,
+    provincia: "Cañar",
+    canton: "Cañar",
+    parroquia: "San Antonio de Paguancay",
+    videoId: "2tkF2SSbCn8",
+    titulo: "Gobierno Parroquial San Antonio de Paguancay"
+  },
+
+  {
+    id: 66,
+    provincia: "Cañar",
+    canton: "Cañar",
+    parroquia: "Honorato Vásquez",
+    videoId: "BA5csorboyw",
+    titulo: "Gobierno Parroquial Honorato Vásquez"
+  },
+
+
 ];
